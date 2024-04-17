e0827e702dd0f4e8ef097b68f0e8fb85b7f8d25e
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-04-17 18:30:48 +0200

    Create randomkiller command

---
+++ commands/fun/randomkiller.js
@@ -0,0 +1,54 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  name: "randomkiller",
+  category: "info",
+  permissions: [],
+  devCommand: false,
+  data: new SlashCommandBuilder()
+    .setName("randomkiller")
+    .setDescription(`Responds with a random killer name`),
+  async execute(interaction) {
+    let killers = [
+      "Trapper",
+      "Wraith",
+      "Hillbilly",
+      "Nurs",
+      "Huntress",
+      "Myers",
+      "Hag",
+      "Doctor",
+      "Pig",
+      "Clown",
+      "Spirit",
+      "Legion",
+      "Plague",
+      "Ghostface",
+      "Demogorgon",
+      "Deathslinger",
+      "Pyramid head",
+      "Blight",
+      "Twins",
+      "Trickster",
+      "Nemesis",
+      "Artist",
+      "Unknown",
+      "Chucky",
+      "Xenomorph",
+      "Singularity",
+      "Skull merchant",
+      "Knight",
+      "Wesker",
+      "Dredge",
+      "Sadako",
+      "Pinhead",
+      "Oni",
+      "Nightmare",
+      "Bubba",
+    ];
+
+    const randomIndex = Math.floor(Math.random() * killers.length);
+    const responseMessage = `Your randomly selected killer is: ${killers[randomIndex]}`;
+    await interaction.reply(responseMessage);
+  },
+};
