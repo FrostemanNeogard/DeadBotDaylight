11fce2c1a1832b3faf20274dcd8554e8b5232fb8
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-04-21 13:22:58 +0200

    Create random addon qualities command

---
+++ commands/fun/randomAddonQualities.js
@@ -0,0 +1,31 @@
+const { SlashCommandBuilder } = require("discord.js");
+
+module.exports = {
+  name: "randomaddons",
+  category: "info",
+  permissions: [],
+  devCommand: false,
+  data: new SlashCommandBuilder()
+    .setName("randomaddons")
+    .setDescription(`Responds with two random addon qualities`),
+  async execute(interaction) {
+    let killers = [
+      "Brown",
+      "Yellow",
+      "Green",
+      "Purple",
+      "Iridescent",
+      "Anniversary",
+    ];
+
+    const addons = [];
+    for (let i = 0; i < 2; i++) {
+      const randomIndex = Math.floor(Math.random() * killers.length);
+      addons.push(killers[randomIndex]);
+    }
+    const responseMessage = `Your randomly selected addon qualities are:\n- ${addons.join(
+      "\n- "
+    )}`;
+    await interaction.reply({ ephemeral: true, content: responseMessage });
+  },
+};
