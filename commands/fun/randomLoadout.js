76681555214637c67b14e6fb62e2f7c76a3a321b
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-04-25 18:52:58 +0200

    Create randomLoadout command

---
+++ commands/fun/randomLoadout.js
@@ -0,0 +1,126 @@
+const { SlashCommandBuilder, EmbedBuilder, Colors } = require("discord.js");
+const {
+  getRandomKiller,
+  getRandomSurvivor,
+  getRandomOffering,
+  getRandomItem,
+  getRandomAddons,
+  getRandomPerks,
+} = require("../../util/functions");
+
+module.exports = {
+  name: "randomloadout",
+  category: "info",
+  permissions: [],
+  devCommand: false,
+  data: new SlashCommandBuilder()
+    .setName("randomloadout")
+    .setDescription(`Responds with a random survivor- or killer loadout`)
+    .addStringOption((option) =>
+      option
+        .setName("role")
+        .setDescription("Killer or survivor perk.")
+        .addChoices(
+          {
+            name: "Survivor",
+            value: "s",
+          },
+          {
+            name: "Killer",
+            value: "k",
+          }
+        )
+        .setRequired(true)
+    ),
+  async execute(interaction) {
+    const isKillerLoadout =
+      interaction.options.getString("role") == "k" ? true : false;
+
+    let randomItem = isKillerLoadout ? null : getRandomItem();
+
+    let items = [
+      "Flashlight",
+      "Sport Flashlight",
+      "Utility Flashlight",
+      "Broken key",
+      "Dull Key",
+      "Skeleton Key",
+      "Map",
+      "Rainbow Map",
+      "Camping Aid Kit",
+      "First Aid Kit",
+      "Emergency Med-Kit",
+      "Ranger Med-Kit",
+      "Worn-Out Tools",
+      "Toolbox",
+      "Commodious Toolbox",
+      "Mechanic's Toolbox",
+      "Alex's Toolbox",
+      "Engineer's Toolbox",
+    ];
+
+    const itemCode = items.slice(0, 3).includes(randomItem)
+      ? "Flashlights"
+      : items.slice(3, 6).includes(randomItem)
+      ? "Keys"
+      : items.slice(6, 8).includes(randomItem)
+      ? "Maps"
+      : items.slice(8, 12).includes(randomItem)
+      ? "Med-Kits"
+      : items.slice(12, 18).includes(randomItem)
+      ? "Toolboxes"
+      : "Toolboxes";
+
+    let randomCharacter = isKillerLoadout
+      ? getRandomKiller()
+      : getRandomSurvivor();
+    let randomOffering = getRandomOffering();
+    const randomPerks = getRandomPerks(isKillerLoadout);
+    const randomAddons = isKillerLoadout
+      ? getRandomAddons(randomCharacter)
+      : getRandomAddons(itemCode);
+
+    console.log(randomCharacter, randomAddons);
+
+    const responseEmbed = new EmbedBuilder()
+      .setColor(Colors.Purple)
+      .setTitle(`Randomized ${isKillerLoadout ? "Killer" : "Survivor"} loadout`)
+      .setFields(
+        {
+          name: "Character",
+          value: randomCharacter,
+          inline: true,
+        },
+        {
+          name: "Offering",
+          value: randomOffering,
+          inline: true,
+        }
+      );
+
+    if (!isKillerLoadout) {
+      responseEmbed.addFields({
+        name: "Item",
+        value: randomItem || "N/A",
+        inline: true,
+      });
+    }
+
+    responseEmbed.addFields(
+      {
+        name: "Perks",
+        value: randomPerks.join("\n"),
+      },
+      {
+        name: "Add-ons",
+        value: randomAddons.join("\n"),
+        inline: true,
+      }
+    );
+
+    await interaction.reply({
+      ephemeral: true,
+      embeds: [responseEmbed],
+    });
+  },
+};
