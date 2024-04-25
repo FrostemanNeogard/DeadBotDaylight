e5413084e6068b9ac6a9309c8a4e4f1fc667a66f
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-04-25 18:52:42 +0200

    Accomodate new "functions" util

--- commands/fun/randomsurvivor.js
+++ commands/fun/randomsurvivor.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require("discord.js");
+const { getRandomSurvivor } = require("../../util/functions");
 
 module.exports = {
   name: "randomsurvivor",
@@ -9,52 +10,8 @@ module.exports = {
     .setName("randomsurvivor")
     .setDescription(`Responds with a random survivor name`),
   async execute(interaction) {
-    let survivors = [
-      "Dwight Fairfield",
-      "Meg Thomas",
-      "Claudette Morel",
-      "Jake Park",
-      "Nea Karlsson",
-      'William "Bill" Overbeck',
-      "David King",
-      "Laurie Strode",
-      "Feng Min",
-      "Detective Tapp",
-      "Kate Densom",
-      "Adam Francis",
-      "Jeff Johansen",
-      "Ashley J. Williams",
-      "Nancy Wheeler",
-      "Steve Harrington",
-      "Zarina Kassir",
-      "Cheryl Manson",
-      "Felix Richter",
-      "Yun-Jin Lee",
-      "Jill Valentine",
-      "Leon S. Kennedy",
-      "Jonah Vasquez",
-      "Gabriel Soma",
-      "Sable Ward",
-      "Alan Wake",
-      "Ellen Ripley",
-      "Nicolas Cage",
-      "Renato Lyra",
-      "Thalita Lyra",
-      "Vittorio Toscano",
-      "Rebecca Chambers",
-      "Ada Wong",
-      "Haddie Kaur",
-      "Yoichi Asakawa",
-      "Mikaela Reid",
-      "Élodie Rakoto",
-      "Yui Kimura",
-      "Jane Romero",
-      "Quentin Smith",
-      "Ace Visconti",
-    ];
-
-    const randomIndex = Math.floor(Math.random() * survivors.length);
-    const responseMessage = `Your randomly selected survivor is: ${survivors[randomIndex]}`;
-    await interaction.reply(responseMessage);
+    const randomSurvivor = getRandomSurvivor();
+    const responseMessage = `Your randomly selected survivor is: ${randomSurvivor}`;
+    await interaction.reply({ ephemeral: true, content: responseMessage });
   },
 };
