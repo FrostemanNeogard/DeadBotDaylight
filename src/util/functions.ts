2789e4e0ebfc1e27bd51d6b9c197318b9b919c9f
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-06-11 10:15:32 +0200

    migrate remaining functions to fetch api from js

--- src/util/functions.ts
+++ src/util/functions.ts
@@ -1,6 +1,9 @@
 import { Addon } from "../__types/addon";
+import { Item } from "../__types/item";
 import { Killer } from "../__types/killer";
+import { Offering } from "../__types/offering";
 import { Perk } from "../__types/perk";
+import { Survivor } from "../__types/survivor";
 
 const { BASE_API_URL } = process.env;
 
@@ -38,4 +41,62 @@ export async function fetchRandomKiller(): Promise<Killer | void> {
     return;
   }
   return killer;
+}
+
+export async function fetchRandomSurvivors(): Promise<Survivor[] | void> {
+  const apiUrl = BASE_API_URL + "survivors/random";
+  const response = await fetch(apiUrl);
+  const data: Survivor[] = await response.json();
+  if (data?.length <= 0) {
+    return;
+  }
+  return data;
+}
+
+export async function fetchRandomAddons(
+  ownerName: string
+): Promise<Addon[] | void> {
+  const apiUrl = BASE_API_URL + `addons/${ownerName}/random`;
+  const response = await fetch(apiUrl);
+  const data: Addon[] = await response.json();
+  if (data?.length <= 0) {
+    return;
+  }
+  return data;
+}
+
+export async function fetchRandomOfferings(
+  isKiller: boolean
+): Promise<Offering[] | void> {
+  const apiUrl =
+    BASE_API_URL + `offerings/random?role=${isKiller ? "killer" : "survivor"}`;
+  const response = await fetch(apiUrl);
+  const data: Offering[] = await response.json();
+  if (data?.length <= 0) {
+    return;
+  }
+  return data;
+}
+
+export async function fetchRandomItem(): Promise<Item[] | void> {
+  const apiUrl = BASE_API_URL + `items/random`;
+  const response = await fetch(apiUrl);
+  const data: Item[] = await response.json();
+  if (data?.length <= 0) {
+    return;
+  }
+  return data;
+}
+
+export async function fetchRandomPerks(
+  isKiller: boolean
+): Promise<Perk[] | void> {
+  const apiUrl =
+    BASE_API_URL + `perks/random?role=${isKiller ? "killer" : "survivor"}`;
+  const response = await fetch(apiUrl);
+  const data: Perk[] = await response.json();
+  if (data?.length <= 0) {
+    return;
+  }
+  return data;
 }
