1ce53eb610badf94846a84ff19fd58c8d3d4e497
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-06-11 09:37:40 +0200

    create perk command

---
+++ src/commands/perk.ts
@@ -0,0 +1,43 @@
+import {
+  ApplicationCommandOptionType,
+  EmbedBuilder,
+  type CommandInteraction,
+} from "discord.js";
+import { Discord, Slash, SlashOption } from "discordx";
+import { fetchPerkData } from "../util/functions";
+import { COLORS } from "../util/config";
+import { Perk } from "../__types/perk";
+
+@Discord()
+export class PerkCommands {
+  @Slash({
+    description: `Replies with information about a given perk.`,
+  })
+  async perk(
+    @SlashOption({
+      name: `perkname`,
+      description: `Perk name to search for`,
+      required: true,
+      type: ApplicationCommandOptionType.String,
+    })
+    perkname: string,
+    interaction: CommandInteraction
+  ): Promise<void> {
+    const perkData: Perk = await fetchPerkData(perkname);
+    const responseEmbed = new EmbedBuilder()
+      .setColor(COLORS.main)
+      .setThumbnail(perkData.image)
+      .setTitle(perkData.name)
+      .setFields(
+        {
+          name: "Description",
+          value: perkData.info,
+        },
+        {
+          name: "Character",
+          value: perkData.character,
+        }
+      );
+    await interaction.reply({ embeds: [responseEmbed] });
+  }
+}
