513300d85c9f77adab22c7276263b54344c92e3d
FrostemanNeogard <liamfrostemanneogard@gmail.com>
2024-06-11 13:06:59 +0200

    add additional error handling and fix return value of fetchRandomKillers

--- src/util/functions.ts
+++ src/util/functions.ts
@@ -13,6 +13,9 @@ export async function fetchPerkData(perkName: string): Promise<Perk | void> {
   }
   const apiUrl = BASE_API_URL + `perks?name=${perkName}`;
   const response = await fetch(apiUrl);
+  if (response.status != 200) {
+    return;
+  }
   const data = await response.json();
   const perk = data[0];
   return perk;
@@ -27,6 +30,9 @@ export async function fetchAddonData(
   }
   const apiUrl = BASE_API_URL + `addons/${ownerName}?name=${addonName}`;
   const response = await fetch(apiUrl);
+  if (response.status != 200) {
+    return;
+  }
   const data = await response.json();
   const addon = data[0];
   return addon;
@@ -35,17 +41,22 @@ export async function fetchAddonData(
 export async function fetchRandomKillers(): Promise<Killer[] | void> {
   const apiUrl = BASE_API_URL + "killers/random";
   const response = await fetch(apiUrl);
-  const data = await response.json();
-  const killer = data[0];
-  if (!killer) {
+  if (response.status != 200) {
     return;
   }
-  return killer;
+  const data: Killer[] = await response.json();
+  if (data?.length <= 0) {
+    return;
+  }
+  return data;
 }
 
 export async function fetchRandomSurvivors(): Promise<Survivor[] | void> {
   const apiUrl = BASE_API_URL + "survivors/random";
   const response = await fetch(apiUrl);
+  if (response.status != 200) {
+    return;
+  }
   const data: Survivor[] = await response.json();
   if (data?.length <= 0) {
     return;
@@ -57,7 +68,11 @@ export async function fetchRandomAddons(
   ownerName: string
 ): Promise<Addon[] | void> {
   const apiUrl = BASE_API_URL + `addons/${ownerName}/random`;
+  console.log(apiUrl);
   const response = await fetch(apiUrl);
+  if (response.status != 200) {
+    return;
+  }
   const data: Addon[] = await response.json();
   if (data?.length <= 0) {
     return;
@@ -71,6 +86,9 @@ export async function fetchRandomOfferings(
   const apiUrl =
     BASE_API_URL + `offerings/random?role=${isKiller ? "killer" : "survivor"}`;
   const response = await fetch(apiUrl);
+  if (response.status != 200) {
+    return;
+  }
   const data: Offering[] = await response.json();
   if (data?.length <= 0) {
     return;
@@ -81,6 +99,9 @@ export async function fetchRandomOfferings(
 export async function fetchRandomItems(): Promise<Item[] | void> {
   const apiUrl = BASE_API_URL + `items/random`;
   const response = await fetch(apiUrl);
+  if (response.status != 200) {
+    return;
+  }
   const data: Item[] = await response.json();
   if (data?.length <= 0) {
     return;
@@ -94,6 +115,9 @@ export async function fetchRandomPerks(
   const apiUrl =
     BASE_API_URL + `perks/random?role=${isKiller ? "killer" : "survivor"}`;
   const response = await fetch(apiUrl);
+  if (response.status != 200) {
+    return;
+  }
   const data: Perk[] = await response.json();
   if (data?.length <= 0) {
     return;
